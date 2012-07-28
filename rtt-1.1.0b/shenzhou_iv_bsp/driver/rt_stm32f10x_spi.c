cda59d3f177003202349ef3cec41d6c4ea2e2f13
darcy.gong <darcy.gong@gmail.com>
2012-07-28 05:11:14 +0000

    1.1.0 SPI 工作不正常

--- rtt-1.1.0b/shenzhou_iv_bsp/driver/rt_stm32f10x_spi.c
+++ rtt-1.1.0b/shenzhou_iv_bsp/driver/rt_stm32f10x_spi.c
@@ -1,44 +1,89 @@
+/*
+ * File      : stm32f20x_40x_spi.c
+ * This file is part of RT-Thread RTOS
+ * COPYRIGHT (C) 2009 RT-Thread Develop Team
+ *
+ * The license and distribution terms for this file may be
+ * found in the file LICENSE in this distribution or at
+ * http://www.rt-thread.org/license/LICENSE
+ *
+ * Change Logs:
+ * Date           Author       Notes
+ * 20012-01-01    aozima       first implementation.
+ */
+
 #include "rt_stm32f10x_spi.h"
 
+/* private rt-thread spi ops function */
 static rt_err_t configure(struct rt_spi_device* device, struct rt_spi_configuration* configuration);
 static rt_uint32_t xfer(struct rt_spi_device* device, struct rt_spi_message* message);
 
-///*
 static struct rt_spi_ops stm32_spi_ops =
 {
     configure,
     xfer
 };
-//*/
 
-#ifdef USING_SPI1
-static struct stm32_spi_bus stm32_spi_bus_1;
-#endif /* #ifdef USING_SPI1 */
+//#ifdef USING_SPI1
+//static struct stm32_spi_bus stm32_spi_bus_1;
+//#endif /* #ifdef USING_SPI1 */
 
-#ifdef USING_SPI2
-static struct stm32_spi_bus stm32_spi_bus_2;
-#endif /* #ifdef USING_SPI2 */
+//#ifdef USING_SPI2
+//static struct stm32_spi_bus stm32_spi_bus_2;
+//#endif /* #ifdef USING_SPI2 */
 
-#ifdef USING_SPI3
-static struct stm32_spi_bus stm32_spi_bus_3;
-#endif /* #ifdef USING_SPI3 */
+//#ifdef USING_SPI3
+//static struct stm32_spi_bus stm32_spi_bus_3;
+//#endif /* #ifdef USING_SPI3 */
 
 //------------------ DMA ------------------
 #ifdef SPI_USE_DMA
 static uint8_t dummy = 0xFF;
-#endif
-
-#ifdef SPI_USE_DMA
-static void DMA_Configuration(struct stm32_spi_bus * stm32_spi_bus, const void * send_addr, void * recv_addr, rt_size_t size)
+static void DMA_RxConfiguration(struct stm32_spi_bus * stm32_spi_bus,
+                                const void * send_addr,
+                                void * recv_addr,
+                                rt_size_t size)
 {
     DMA_InitTypeDef DMA_InitStructure;
 
+	/*
+    // Reset DMA Stream registers (for debug purpose) 
+    DMA_DeInit(stm32_spi_bus->DMA_Stream_RX);
+    DMA_DeInit(stm32_spi_bus->DMA_Stream_TX);
+
+    // Check if the DMA Stream is disabled before enabling it.
+    //   Note that this step is useful when the same Stream is used multiple times:
+    //   enabled, then disabled then re-enabled... In this case, the DMA Stream disable
+    //   will be effective only at the end of the ongoing data transfer and it will
+    //   not be possible to re-configure it before making sure that the Enable bit
+    //   has been cleared by hardware. If the Stream is used only once, this step might
+    //   be bypassed. 
+    while (DMA_GetCmdStatus(stm32_spi_bus->DMA_Stream_RX) != DISABLE);
+    while (DMA_GetCmdStatus(stm32_spi_bus->DMA_Stream_TX) != DISABLE);
+
+    // Configure DMA_RX Stream 
+    DMA_Cmd(stm32_spi_bus->DMA_Stream_RX, DISABLE);
+	*/
     DMA_ClearFlag(stm32_spi_bus->DMA_Channel_RX_FLAG_TC
                   | stm32_spi_bus->DMA_Channel_RX_FLAG_TE
                   | stm32_spi_bus->DMA_Channel_TX_FLAG_TC
                   | stm32_spi_bus->DMA_Channel_TX_FLAG_TE);
-
-    /* RX channel configuration */
+	/*
+    DMA_InitStructure.DMA_Channel = stm32_spi_bus->DMA_Channel_RX;
+    DMA_InitStructure.DMA_PeripheralBaseAddr = (uint32_t)(&(stm32_spi_bus->SPI->DR));
+    DMA_InitStructure.DMA_DIR = DMA_DIR_PeripheralToMemory;
+    DMA_InitStructure.DMA_BufferSize = (uint32_t)size;
+    DMA_InitStructure.DMA_PeripheralInc = DMA_PeripheralInc_Disable;
+    DMA_InitStructure.DMA_PeripheralDataSize = DMA_PeripheralDataSize_Byte;
+    DMA_InitStructure.DMA_MemoryDataSize = DMA_MemoryDataSize_Byte;
+    DMA_InitStructure.DMA_Mode = DMA_Mode_Normal;
+    DMA_InitStructure.DMA_Priority = DMA_Priority_High;
+    DMA_InitStructure.DMA_FIFOMode = DMA_FIFOMode_Disable;
+    DMA_InitStructure.DMA_FIFOThreshold = DMA_FIFOThreshold_Full;
+    DMA_InitStructure.DMA_MemoryBurst = DMA_MemoryBurst_Single;
+    DMA_InitStructure.DMA_PeripheralBurst = DMA_PeripheralBurst_Single;
+	*/
+	/* RX channel configuration */
     DMA_Cmd(stm32_spi_bus->DMA_Channel_RX, DISABLE);
     DMA_InitStructure.DMA_PeripheralBaseAddr = (u32)(&(stm32_spi_bus->SPI->DR));
     DMA_InitStructure.DMA_DIR = DMA_DIR_PeripheralSRC;
@@ -51,14 +96,15 @@ static void DMA_Configuration(struct stm32_spi_bus * stm32_spi_bus, const void *
 
     DMA_InitStructure.DMA_BufferSize = size;
 
+
     if(recv_addr != RT_NULL)
     {
-        DMA_InitStructure.DMA_MemoryBaseAddr = (u32) recv_addr;
+        DMA_InitStructure.DMA_Memory0BaseAddr = (uint32_t) recv_addr;
         DMA_InitStructure.DMA_MemoryInc = DMA_MemoryInc_Enable;
     }
     else
     {
-        DMA_InitStructure.DMA_MemoryBaseAddr = (u32) (&dummy);
+        DMA_InitStructure.DMA_Memory0BaseAddr = (uint32_t) (&dummy);
         DMA_InitStructure.DMA_MemoryInc = DMA_MemoryInc_Disable;
     }
 
@@ -66,7 +112,24 @@ static void DMA_Configuration(struct stm32_spi_bus * stm32_spi_bus, const void *
 
     DMA_Cmd(stm32_spi_bus->DMA_Channel_RX, ENABLE);
 
-    /* TX channel configuration */
+    /* Configure DMA_TX Stream */
+    /*
+    DMA_Cmd(stm32_spi_bus->DMA_Stream_TX, DISABLE);
+
+    DMA_InitStructure.DMA_Channel = stm32_spi_bus->DMA_Channel_TX;
+    DMA_InitStructure.DMA_PeripheralBaseAddr = (uint32_t)(&(stm32_spi_bus->SPI->DR));
+    DMA_InitStructure.DMA_DIR = DMA_DIR_MemoryToPeripheral;
+    DMA_InitStructure.DMA_BufferSize = (uint32_t)size;
+    DMA_InitStructure.DMA_PeripheralInc = DMA_PeripheralInc_Disable;
+    DMA_InitStructure.DMA_PeripheralDataSize = DMA_PeripheralDataSize_Byte;
+    DMA_InitStructure.DMA_MemoryDataSize = DMA_MemoryDataSize_Byte;
+    DMA_InitStructure.DMA_Mode = DMA_Mode_Normal;
+    DMA_InitStructure.DMA_Priority = DMA_Priority_High;
+    DMA_InitStructure.DMA_FIFOMode = DMA_FIFOMode_Disable;
+    DMA_InitStructure.DMA_FIFOThreshold = DMA_FIFOThreshold_Full;
+    DMA_InitStructure.DMA_MemoryBurst = DMA_MemoryBurst_Single;
+    DMA_InitStructure.DMA_PeripheralBurst = DMA_PeripheralBurst_Single;
+	*/
     DMA_Cmd(stm32_spi_bus->DMA_Channel_TX, DISABLE);
     DMA_InitStructure.DMA_PeripheralBaseAddr = (u32)(&(stm32_spi_bus->SPI->DR));
     DMA_InitStructure.DMA_DIR = DMA_DIR_PeripheralDST;
@@ -79,14 +142,16 @@ static void DMA_Configuration(struct stm32_spi_bus * stm32_spi_bus, const void *
 
     DMA_InitStructure.DMA_BufferSize = size;
 
+	
     if(send_addr != RT_NULL)
     {
-        DMA_InitStructure.DMA_MemoryBaseAddr = (u32)send_addr;
+        DMA_InitStructure.DMA_Memory0BaseAddr = (uint32_t)send_addr;
         DMA_InitStructure.DMA_MemoryInc = DMA_MemoryInc_Enable;
     }
     else
     {
-        DMA_InitStructure.DMA_MemoryBaseAddr = (u32)(&dummy);;
+        dummy = 0xFF;
+        DMA_InitStructure.DMA_Memory0BaseAddr = (uint32_t)(&dummy);;
         DMA_InitStructure.DMA_MemoryInc = DMA_MemoryInc_Disable;
     }
 
@@ -96,53 +161,13 @@ static void DMA_Configuration(struct stm32_spi_bus * stm32_spi_bus, const void *
 }
 #endif
 
-rt_inline uint16_t get_spi_BaudRatePrescaler(rt_uint32_t max_hz)
-{
-    uint16_t SPI_BaudRatePrescaler;
-
-    /* STM32F10x SPI MAX 18Mhz */
-    if(max_hz >= SystemCoreClock/2 && SystemCoreClock/2 <= 18000000)
-    {
-        SPI_BaudRatePrescaler = SPI_BaudRatePrescaler_2;
-    }
-    else if(max_hz >= SystemCoreClock/4)
-    {
-        SPI_BaudRatePrescaler = SPI_BaudRatePrescaler_4;
-    }
-    else if(max_hz >= SystemCoreClock/8)
-    {
-        SPI_BaudRatePrescaler = SPI_BaudRatePrescaler_8;
-    }
-    else if(max_hz >= SystemCoreClock/16)
-    {
-        SPI_BaudRatePrescaler = SPI_BaudRatePrescaler_16;
-    }
-    else if(max_hz >= SystemCoreClock/32)
-    {
-        SPI_BaudRatePrescaler = SPI_BaudRatePrescaler_32;
-    }
-    else if(max_hz >= SystemCoreClock/64)
-    {
-        SPI_BaudRatePrescaler = SPI_BaudRatePrescaler_64;
-    }
-    else if(max_hz >= SystemCoreClock/128)
-    {
-        SPI_BaudRatePrescaler = SPI_BaudRatePrescaler_128;
-    }
-    else
-    {
-        /* min prescaler 256 */
-        SPI_BaudRatePrescaler = SPI_BaudRatePrescaler_256;
-    }
-
-    return SPI_BaudRatePrescaler;
-}
-
-static rt_err_t configure(struct rt_spi_device* device, struct rt_spi_configuration* configuration)
+static rt_err_t configure(struct rt_spi_device* device,
+                          struct rt_spi_configuration* configuration)
 {
     struct stm32_spi_bus * stm32_spi_bus = (struct stm32_spi_bus *)device->bus;
     SPI_InitTypeDef SPI_InitStructure;
 
+	SPI_StructInit(&SPI_InitStructure);
     /* data_width */
     if(configuration->data_width <= 8)
     {
@@ -156,8 +181,59 @@ static rt_err_t configure(struct rt_spi_device* device, struct rt_spi_configurat
     {
         return RT_EIO;
     }
+
     /* baudrate */
-    SPI_InitStructure.SPI_BaudRatePrescaler = get_spi_BaudRatePrescaler(configuration->max_hz);
+    {
+        uint32_t SPI_APB_CLOCK;
+        uint32_t stm32_spi_max_clock;
+        uint32_t max_hz;
+
+        stm32_spi_max_clock = 18000000;
+        max_hz = configuration->max_hz;
+
+        if(max_hz > stm32_spi_max_clock)
+        {
+            max_hz = stm32_spi_max_clock;
+        }
+
+        SPI_APB_CLOCK = SystemCoreClock / 4;
+
+        if(max_hz >= SPI_APB_CLOCK/2 && SPI_APB_CLOCK/2 <= 18000000)
+        {
+            SPI_InitStructure.SPI_BaudRatePrescaler = SPI_BaudRatePrescaler_2;
+        }
+        else if(max_hz >= SPI_APB_CLOCK/4)
+        {
+            SPI_InitStructure.SPI_BaudRatePrescaler = SPI_BaudRatePrescaler_4;
+        }
+        else if(max_hz >= SPI_APB_CLOCK/8)
+        {
+            SPI_InitStructure.SPI_BaudRatePrescaler = SPI_BaudRatePrescaler_8;
+        }
+        else if(max_hz >= SPI_APB_CLOCK/16)
+        {
+            SPI_InitStructure.SPI_BaudRatePrescaler = SPI_BaudRatePrescaler_16;
+        }
+        else if(max_hz >= SPI_APB_CLOCK/32)
+        {
+            SPI_InitStructure.SPI_BaudRatePrescaler = SPI_BaudRatePrescaler_32;
+        }
+        else if(max_hz >= SPI_APB_CLOCK/64)
+        {
+            SPI_InitStructure.SPI_BaudRatePrescaler = SPI_BaudRatePrescaler_64;
+        }
+        else if(max_hz >= SPI_APB_CLOCK/128)
+        {
+            SPI_InitStructure.SPI_BaudRatePrescaler = SPI_BaudRatePrescaler_128;
+        }
+        else
+        {
+            /*  min prescaler 256 */
+            SPI_InitStructure.SPI_BaudRatePrescaler = SPI_BaudRatePrescaler_256;
+        }
+	}
+
+
     /* CPOL */
     if(configuration->mode & RT_SPI_CPOL)
     {
@@ -218,14 +294,17 @@ static rt_uint32_t xfer(struct rt_spi_device* device, struct rt_spi_message* mes
     {
         if(config->data_width <= 8)
         {
-            DMA_Configuration(stm32_spi_bus, message->send_buf, message->recv_buf, message->length);
+            DMA_RxConfiguration(stm32_spi_bus, message->send_buf, message->recv_buf, message->length);
+//            SPI_I2S_ClearFlag(SPI, SPI_I2S_FLAG_RXNE);
             SPI_I2S_DMACmd(SPI, SPI_I2S_DMAReq_Tx | SPI_I2S_DMAReq_Rx, ENABLE);
+            /*
+            while (DMA_GetFlagStatus(stm32_spi_bus->DMA_Stream_RX, stm32_spi_bus->DMA_Channel_RX_FLAG_TC) == RESET
+                    || DMA_GetFlagStatus(stm32_spi_bus->DMA_Stream_TX, stm32_spi_bus->DMA_Channel_TX_FLAG_TC) == RESET);
+            */
             while (DMA_GetFlagStatus(stm32_spi_bus->DMA_Channel_RX_FLAG_TC) == RESET
-                    || DMA_GetFlagStatus(stm32_spi_bus->DMA_Channel_TX_FLAG_TC) == RESET);
+            		|| DMA_GetFlagStatus(stm32_spi_bus->DMA_Channel_TX_FLAG_TC) == RESET);
             SPI_I2S_DMACmd(SPI, SPI_I2S_DMAReq_Tx | SPI_I2S_DMAReq_Rx, DISABLE);
         }
-//        rt_memcpy(buffer,_spi_flash_buffer,DMA_BUFFER_SIZE);
-//        buffer += DMA_BUFFER_SIZE;
     }
     else
 #endif
@@ -301,11 +380,121 @@ static rt_uint32_t xfer(struct rt_spi_device* device, struct rt_spi_message* mes
     return message->length;
 };
 
+/** \brief init and register stm32 spi bus.
+ *
+ * \param SPI: STM32 SPI, e.g: SPI1,SPI2,SPI3.
+ * \param stm32_spi: stm32 spi bus struct.
+ * \param spi_bus_name: spi bus name, e.g: "spi1"
+ * \return
+ *
+ */
+rt_err_t stm32_spi_register(SPI_TypeDef * SPI,
+                            struct stm32_spi_bus * stm32_spi,
+                            const char * spi_bus_name)
+{
+	rt_kprintf("stm32_spi_register ");
+    if(SPI == SPI1)
+    {
+		rt_kprintf("SPI1 ");
+/*
+#ifdef SPI_USE_DMA
+        RCC_AHBPeriphClockCmd(RCC_AHBPeriph_DMA1, ENABLE);
+        // DMA2_Stream0 DMA_Channel_3 : SPI1_RX
+        // DMA2_Stream2 DMA_Channel_3 : SPI1_RX
+        stm32_spi->DMA_Stream_RX = DMA1_Stream0;
+        stm32_spi->DMA_Channel_RX = DMA_Channel_3;
+        stm32_spi->DMA_Channel_RX_FLAG_TC = DMA_FLAG_TCIF0;
+        /// DMA2_Stream3 DMA_Channel_3 : SPI1_TX 
+        /// DMA2_Stream5 DMA_Channel_3 : SPI1_TX 
+        stm32_spi->DMA_Stream_TX = DMA1_Stream3;
+        stm32_spi->DMA_Channel_TX = DMA_Channel_3;
+        stm32_spi->DMA_Channel_TX_FLAG_TC = DMA_FLAG_TCIF3;
+#endif
+        RCC_APB2PeriphClockCmd(RCC_APB2Periph_SPI1, ENABLE);
+*/
+        stm32_spi->SPI = SPI;
+#ifdef SPI_USE_DMA
+		rt_kprintf("DMA \r\n");
+        stm32_spi->DMA_Channel_RX = DMA1_Channel2;
+        stm32_spi->DMA_Channel_TX = DMA1_Channel3;
+        stm32_spi->DMA_Channel_RX_FLAG_TC = DMA1_FLAG_TC2;
+        stm32_spi->DMA_Channel_RX_FLAG_TE = DMA1_FLAG_TE2;
+        stm32_spi->DMA_Channel_TX_FLAG_TC = DMA1_FLAG_TC3;
+        stm32_spi->DMA_Channel_TX_FLAG_TE = DMA1_FLAG_TE3;
+#endif /* #ifdef SPI_USE_DMA */
+        //rt_spi_bus_register(&stm32_spi->parent, "spi1", &stm32_spi_ops);
+    }
+    else if(SPI == SPI2)
+    {
+/*
+        stm32_spi->SPI = SPI2;
+#ifdef SPI_USE_DMA
+        RCC_AHB1PeriphClockCmd(RCC_AHB1Periph_DMA1, ENABLE);
+        // DMA1_Stream3 DMA_Channel_0 : SPI2_RX
+        stm32_spi->DMA_Stream_RX = DMA1_Stream3;
+        stm32_spi->DMA_Channel_RX = DMA_Channel_0;
+        stm32_spi->DMA_Channel_RX_FLAG_TC = DMA_FLAG_TCIF3;
+        // DMA1_Stream4 DMA_Channel_0 : SPI2_TX 
+        stm32_spi->DMA_Stream_TX = DMA1_Stream4;
+        stm32_spi->DMA_Channel_TX = DMA_Channel_0;
+        stm32_spi->DMA_Channel_TX_FLAG_TC = DMA_FLAG_TCIF4;
+#endif
+        RCC_APB1PeriphClockCmd(RCC_APB1Periph_SPI2, ENABLE);
+*/
+        stm32_spi->SPI = SPI;
+#ifdef SPI_USE_DMA
+        stm32_spi->DMA_Channel_RX = DMA1_Channel4;
+        stm32_spi->DMA_Channel_TX = DMA1_Channel5;
+        stm32_spi->DMA_Channel_RX_FLAG_TC = DMA1_FLAG_TC4;
+        stm32_spi->DMA_Channel_RX_FLAG_TE = DMA1_FLAG_TE4;
+        stm32_spi->DMA_Channel_TX_FLAG_TC = DMA1_FLAG_TC5;
+        stm32_spi->DMA_Channel_TX_FLAG_TE = DMA1_FLAG_TE5;
+#endif /* #ifdef SPI_USE_DMA */
+        //rt_spi_bus_register(&stm32_spi->parent, "spi2", &stm32_spi_ops);
+    }
+    else if(SPI == SPI3)
+    {
+/*
+#ifdef SPI_USE_DMA
+        RCC_AHB1PeriphClockCmd(RCC_AHB1Periph_DMA1, ENABLE);
+        // DMA1_Stream2 DMA_Channel_0 : SPI3_RX 
+        stm32_spi->DMA_Stream_RX = DMA1_Stream2;
+        stm32_spi->DMA_Channel_RX = DMA_Channel_0;
+        stm32_spi->DMA_Channel_RX_FLAG_TC = DMA_FLAG_TCIF2;
+        // DMA1_Stream5 DMA_Channel_0 : SPI3_TX 
+        stm32_spi->DMA_Stream_TX = DMA1_Stream5;
+        stm32_spi->DMA_Channel_TX = DMA_Channel_0;
+        stm32_spi->DMA_Channel_TX_FLAG_TC = DMA_FLAG_TCIF5;
+#endif
+        RCC_APB1PeriphClockCmd(RCC_APB1Periph_SPI3, ENABLE);
+*/
+        stm32_spi->SPI = SPI;
+#ifdef SPI_USE_DMA
+        stm32_spi->DMA_Channel_RX = DMA2_Channel1;
+        stm32_spi->DMA_Channel_TX = DMA2_Channel2;
+        stm32_spi->DMA_Channel_RX_FLAG_TC = DMA2_FLAG_TC1;
+        stm32_spi->DMA_Channel_RX_FLAG_TE = DMA2_FLAG_TE1;
+        stm32_spi->DMA_Channel_TX_FLAG_TC = DMA2_FLAG_TC2;
+        stm32_spi->DMA_Channel_TX_FLAG_TE = DMA2_FLAG_TE2;
+#endif /* #ifdef SPI_USE_DMA */
+        //rt_spi_bus_register(&stm32_spi->parent, "spi3", &stm32_spi_ops);
+    }
+    else
+    {
+        return RT_ENOSYS;
+    }
+
+    return rt_spi_bus_register(&stm32_spi->parent, spi_bus_name, &stm32_spi_ops);
+}
+
 void rt_stm32f10x_spi_init(void)
 {
+rt_kprintf("rt_stm32f10x_spi_init \r\n");
 #ifdef USING_SPI1
     /* SPI1 config */
     {
+		//rt_kprintf("USE_SPI1 \r\n");
+		static struct stm32_spi_bus stm32_spi_1;
         GPIO_InitTypeDef GPIO_InitStructure;
 
         /* Enable SPI1 Periph clock */
@@ -314,6 +503,7 @@ void rt_stm32f10x_spi_init(void)
         ENABLE);
 
 #ifdef SPI_USE_DMA
+		rt_kprintf("SPI_USE_DMA \r\n");
         /* Enable the DMA1 Clock */
         RCC_AHBPeriphClockCmd(RCC_AHBPeriph_DMA1, ENABLE);
 #endif /* #ifdef SPI_USE_DMA */
@@ -323,26 +513,17 @@ void rt_stm32f10x_spi_init(void)
         GPIO_InitStructure.GPIO_Speed = GPIO_Speed_50MHz;
         GPIO_InitStructure.GPIO_Mode = GPIO_Mode_AF_PP;
         GPIO_Init(GPIOA, &GPIO_InitStructure);
+
+		stm32_spi_register(SPI1, &stm32_spi_1, "spi1");
     } /* SPI1 config */
 
-    /* register SPI1 */
-    {
-        stm32_spi_bus_1.SPI = SPI1;
-#ifdef SPI_USE_DMA
-        stm32_spi_bus_1.DMA_Channel_RX = DMA1_Channel2;
-        stm32_spi_bus_1.DMA_Channel_TX = DMA1_Channel3;
-        stm32_spi_bus_1.DMA_Channel_RX_FLAG_TC = DMA1_FLAG_TC2;
-        stm32_spi_bus_1.DMA_Channel_RX_FLAG_TE = DMA1_FLAG_TE2;
-        stm32_spi_bus_1.DMA_Channel_TX_FLAG_TC = DMA1_FLAG_TC3;
-        stm32_spi_bus_1.DMA_Channel_TX_FLAG_TE = DMA1_FLAG_TE3;
-#endif /* #ifdef SPI_USE_DMA */
-        rt_spi_bus_register(&stm32_spi_bus_1.parent, "spi1", &stm32_spi_ops);
-    } /* register SPI1 */
-#endif /* #ifdef USING_SPI1 */
+	
+#endif
 
 #ifdef USING_SPI2
     /* SPI config */
     {
+		static struct stm32_spi_bus stm32_spi_2;
         GPIO_InitTypeDef GPIO_InitStructure;
 
         /* Enable SPI2 Periph clock */
@@ -369,26 +550,15 @@ void rt_stm32f10x_spi_init(void)
         /*!< SPI MOSI pin configuration */
         GPIO_InitStructure.GPIO_Pin =  GPIO_Pin_15; /* PB15 SPI2_MOSI */
         GPIO_Init(GPIOB, &GPIO_InitStructure);
-    } /* SPI config */
 
-    /* register SPI */
-    {
-        stm32_spi_bus_2.SPI = SPI2;
-#ifdef SPI_USE_DMA
-        stm32_spi_bus_2.DMA_Channel_RX = DMA1_Channel4;
-        stm32_spi_bus_2.DMA_Channel_TX = DMA1_Channel5;
-        stm32_spi_bus_2.DMA_Channel_RX_FLAG_TC = DMA1_FLAG_TC4;
-        stm32_spi_bus_2.DMA_Channel_RX_FLAG_TE = DMA1_FLAG_TE4;
-        stm32_spi_bus_2.DMA_Channel_TX_FLAG_TC = DMA1_FLAG_TC5;
-        stm32_spi_bus_2.DMA_Channel_TX_FLAG_TE = DMA1_FLAG_TE5;
-#endif /* #ifdef SPI_USE_DMA */
-        rt_spi_bus_register(&stm32_spi_bus_2.parent, "spi2", &stm32_spi_ops);
-    }/* register SPI */
-#endif /* #ifdef USING_SPI2 */
+		stm32_spi_register(SPI2, &stm32_spi_2, "spi2");
+    } /* SPI config */
+#endif
 
 #ifdef USING_SPI3
     /* SPI config */
     {
+		static struct stm32_spi_bus stm32_spi_3;
         GPIO_InitTypeDef GPIO_InitStructure;
 
         /* Enable SPI2 Periph clock */
@@ -415,20 +585,9 @@ void rt_stm32f10x_spi_init(void)
         /*!< SPI MOSI pin configuration */
         GPIO_InitStructure.GPIO_Pin =  GPIO_Pin_15; /* PB15 SPI2_MOSI */
         GPIO_Init(GPIOB, &GPIO_InitStructure);
-    } /* SPI config */
 
-    /* register SPI */
-    {
-        stm32_spi_bus_3.SPI = SPI2;
-#ifdef SPI_USE_DMA
-        stm32_spi_bus_3.DMA_Channel_RX = DMA2_Channel1;
-        stm32_spi_bus_3.DMA_Channel_TX = DMA2_Channel2;
-        stm32_spi_bus_3.DMA_Channel_RX_FLAG_TC = DMA2_FLAG_TC1;
-        stm32_spi_bus_3.DMA_Channel_RX_FLAG_TE = DMA2_FLAG_TE1;
-        stm32_spi_bus_3.DMA_Channel_TX_FLAG_TC = DMA2_FLAG_TC2;
-        stm32_spi_bus_3.DMA_Channel_TX_FLAG_TE = DMA2_FLAG_TE2;
-#endif /* #ifdef SPI_USE_DMA */
-        rt_spi_bus_register(&stm32_spi_bus_3.parent, "spi3", &stm32_spi_ops);
-    }/* register SPI */
-#endif /* #ifdef USING_SPI3 */
+		stm32_spi_register(SPI3, &stm32_spi_3, "spi3");
+    } /* SPI config */
+#endif
+
 }
